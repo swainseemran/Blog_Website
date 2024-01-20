1e8de70023d6e4f540e2ae637c33bf0e7d298dc0
swainseemran <swainseemran@gmail.com>
2024-01-20 20:29:44 +0530

    basic blog website

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const app = express()
+
+const password = 'IlqNQvruYoykPrSY';
+mongoose.connect(`mongodb+srv://Atlas:${password}@cluster0.rnylb6z.mongodb.net/blog?retryWrites=true&w=majority`, {
+  useNewUrlParser: true, useUnifiedTopology: true,
+})
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
+
+app.use('/articles', articleRouter)
+
+app.listen(5000)
